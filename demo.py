f316b0996355a99237fc61ad26eb193a13801408
Meet Rathod <meet93033@gmail.com>
2025-12-04 20:15:08 +0530

    Add files via upload
    Signed-off-by: Meet Rathod  <meet93033@gmail.com>

---
+++ demo.py
@@ -0,0 +1,7 @@
+#Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+#Enter "help" below or click "Help" above for more information.
+a=2.5+2.5j
+b=3.0-0.5j
+c=a+b
+print("Sum=",c);
+
